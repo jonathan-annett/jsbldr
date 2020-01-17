3e17f8f9f978ae4ed5bed11627d9c837a86bdf00
jonathan-annett <jonathan.max.annett@gmail.com>
2020-01-17 17:13:15 +1100

    changed repo for extensions/polyfills

--- update_ext.sh
+++ update_ext.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-
-wget https://raw.githubusercontent.com/jonathan-annett/tab-calls/master/src/polyfills.min.js -q -O ./polyfills.min.js
-wget https://raw.githubusercontent.com/jonathan-annett/tab-calls/master/src/extensions.min.js -q -O ./extensions.min.js
+wget https://raw.githubusercontent.com/jonathan-annett/jsextensions/master/src/polyfills.min.js -q -O ./polyfills.min.js
+wget https://raw.githubusercontent.com/jonathan-annett/jsextensions/master/src/extensions.min.js -q -O ./extensions.min.js
