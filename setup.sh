47c313d576601be82afcbdabe92a4780251cfa10
jonathan-annett <jonathan.max.annett@gmail.com>
2020-01-17 17:31:26 +1100

    initial check in

---
+++ setup.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+if which node >/dev/null; then
+echo -n node ok:
+node --version
+else 
+echo please install node.js
+echo eg: 
+echo curl -L https://raw.githubusercontent.com/tj/n/master/bin/n -o n
+echo bash n lts
+fi
+if which uglifyjs >/dev/null; then
+echo -n uglifyjs ok:
+uglifyjs --version
+else
+echo please install uglifyjs
+echo eg:   npm install -g uglify-js
+fi
+
+npm install uglify-js jshint
