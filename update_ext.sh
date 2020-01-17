2b7eb8835ff2dda68791bb2cdf0413513ebfaa05
jonathan-annett <jonathan.max.annett@gmail.com>
2020-01-17 16:14:27 +1100

    initial check in

---
+++ update_ext.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+wget https://raw.githubusercontent.com/jonathan-annett/tab-calls/master/src/polyfills.min.js -q -O ./polyfills.min.js
+wget https://raw.githubusercontent.com/jonathan-annett/tab-calls/master/src/extensions.min.js -q -O ./extensions.min.js
